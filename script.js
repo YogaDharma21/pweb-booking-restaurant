565d8ea58e97d4230e0b29ed293ab75c8ec8ab88
komangyxy-commits <ikomangriskytriyanaputra@gmail.com>
2025-12-17 09:31:10 +0800

    menggantiemail di email dan nama title

--- script.js
+++ script.js
@@ -77,7 +77,6 @@ function bookingSection() {
 
 function booking() {
     let Name = document.getElementById("name").value;
-    let Email = document.getElementById("email").value;
     let Date = document.getElementById("date").value;
     let Time = document.getElementById("time").value;
     let Guests = document.getElementById("guests").value;
